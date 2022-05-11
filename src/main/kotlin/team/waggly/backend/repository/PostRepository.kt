ea574e8b812a2c156b95963df6e8b56d04559548
59 <5.9.devv@gmail.com>
2022-05-11 13:34:03 +0900

    post-function v1.0

---
+++ src/main/kotlin/team/waggly/backend/repository/PostRepository.kt
@@ -0,0 +1,8 @@
+package team.waggly.backend.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import team.waggly.backend.model.Post
+
+@Repository
+interface PostRepository: JpaRepository<Post, Long>
