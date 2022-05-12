ec8dadece9189a8d1cd840da3042e4a5c2056f00
SeongeunYang <tnvjaos1826@gmail.com>
2022-05-13 01:11:37 +0900

    merge complete

--- src/main/kotlin/team/waggly/backend/repository/UserRepository.kt
+++ src/main/kotlin/team/waggly/backend/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.waggly.backend.model.User
 
 interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(email: String): User?
 }
