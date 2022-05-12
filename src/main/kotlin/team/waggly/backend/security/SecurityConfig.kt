b4453e53114cfba0ba348703a178a1db2cd16d76
SeongeunYang <tnvjaos1826@gmail.com>
2022-05-13 01:11:37 +0900

    Spring Security Login 로직 구현 완료

---
+++ src/main/kotlin/team/waggly/backend/security/SecurityConfig.kt
@@ -0,0 +1,59 @@
+package team.waggly.backend.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import team.waggly.backend.security.filter.FormLoginFilter
+import team.waggly.backend.security.provider.FormLoginAuthProvider
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(private val userDetailsServiceImpl: UserDetailsServiceImpl) : WebSecurityConfigurerAdapter() {
+    @Bean
+    fun encodePassword() : BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.authenticationProvider(FormLoginAuthProvider(userDetailsServiceImpl, encodePassword()))
+    }
+
+    override fun configure(web: WebSecurity) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**")
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable().httpBasic()
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http
+                .addFilterBefore(formLoginFilter(), UsernamePasswordAuthenticationFilter::class.java)
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**", "/user/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+    }
+
+    @Bean
+    fun formLoginFilter() : FormLoginFilter {
+        val formLoginFilter = FormLoginFilter(authenticationManager())
+        formLoginFilter.setFilterProcessesUrl("/user/login")
+        formLoginFilter.setAuthenticationSuccessHandler(FormLoginSuccessHandler())
+        formLoginFilter.afterPropertiesSet()
+        return formLoginFilter
+    }
+}
--- src/main/kotlin/team/waggly/backend/service/SignupService.kt
+++ src/main/kotlin/team/waggly/backend/service/SignupService.kt
@@ -1,16 +1,17 @@
 package team.waggly.backend.service
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import team.waggly.backend.dto.SignupDto
 import team.waggly.backend.model.User
-import team.waggly.backend.repository.UserRespository
+import team.waggly.backend.repository.UserRepository
 
 @Service
-class SignupService(private val userRespository: UserRespository){
+class SignupService(private val userRepository: UserRepository){
+    private val passwordEncoder = BCryptPasswordEncoder()
+
     fun userSignup(signupRequestDto: SignupDto.Request): SignupDto.Response {
         val initProfileImg = "기본이미지 Url"
         val gender = when (signupRequestDto.gender) {
@@ -19,7 +20,7 @@ class SignupService(private val userRespository: UserRespository){
             else -> throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"성별데이터가 잘못되었습니다.")
         }
         // 스프링 시큐리티에서 passwordEncoder로 인코딩후 넣어준다.
-        val encodedPassword = signupRequestDto.password
+        val encodedPassword = passwordEncoder.encode(signupRequestDto.password)
         print(User.GenderType.FEMALE)
         print(gender)
         val newUser = User(
@@ -36,7 +37,7 @@ class SignupService(private val userRespository: UserRespository){
             null
         )
 
-        userRespository.save(newUser)
+        userRepository.save(newUser)
         return SignupDto.Response(true)
     }
 
