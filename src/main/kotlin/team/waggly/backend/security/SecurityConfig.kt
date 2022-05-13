3b4c46e3930417b396debc0b691baab5c041c13f
SeongeunYang <tnvjaos1826@gmail.com>
2022-05-13 22:58:52 +0900

    Cofig 설정 변경

--- src/main/kotlin/team/waggly/backend/security/SecurityConfig.kt
+++ src/main/kotlin/team/waggly/backend/security/SecurityConfig.kt
@@ -55,7 +55,7 @@ class SecurityConfig(
 
         http.authorizeRequests()
                 .antMatchers("/h2-console/**", "/user/signup").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .exceptionHandling()
     }
