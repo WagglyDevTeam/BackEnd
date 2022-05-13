68e18c6ef8b00f62dc3c48345681d6670d6fdc81
SeongeunYang <tnvjaos1826@gmail.com>
2022-05-13 20:27:09 +0900

    merge affdf44

---
+++ src/main/kotlin/team/waggly/backend/security/jwt/JwtTokenUtils.kt
@@ -0,0 +1,35 @@
+package team.waggly.backend.security.jwt
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm
+import team.waggly.backend.security.UserDetailsImpl
+import java.util.*
+
+class JwtTokenUtils {
+    private val SEC = 1
+    private val MINUTE = 60 * SEC
+    private val HOUR = 60 * MINUTE
+    private val DAY = 24 * HOUR
+
+    private val JWT_TOKEN_VALID_SEC = 3 * DAY
+    private val JWT_TOKEN_VALID_MILLI_SEC = JWT_TOKEN_VALID_SEC * 1000
+
+    internal val CLAIM_EXPIRED_DATE = "EXPIRED_DATE"
+    internal val CLAIM_USER_NAME = "USER_NAME"
+    internal val JWT_SECRET = "wagglybackend_!@#$%"
+
+    fun generateJwtToken(userDetailsImpl: UserDetailsImpl): String? {
+        var token: String? = null;
+        try {
+            token = JWT.create()
+                    .withIssuer("waggly")
+                    .withClaim(CLAIM_USER_NAME, userDetailsImpl.username)
+                    .withClaim(CLAIM_EXPIRED_DATE, Date(System.currentTimeMillis() + JWT_TOKEN_VALID_MILLI_SEC))
+                    .sign(Algorithm.HMAC256(JWT_SECRET)) as String
+        } catch (e: Exception){
+            println(e.message)
+        }
+
+        return token
+    }
+}
