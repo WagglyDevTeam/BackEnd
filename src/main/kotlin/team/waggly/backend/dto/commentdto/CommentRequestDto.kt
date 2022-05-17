c353788b87b4bdd795d4f41f2676074e86c06c71
suky0713@kyonngi.ac.kr <suky0713@kyonggi.ac.kr>
2022-05-17 21:58:59 +0900

    CommentLike

--- src/main/kotlin/team/waggly/backend/dto/commentdto/CommentRequestDto.kt
+++ src/main/kotlin/team/waggly/backend/dto/commentdto/CommentRequestDto.kt
@@ -2,5 +2,5 @@ package team.waggly.backend.dto.commentdto
 
 data class CommentRequestDto(
     val commentDesc : String,
-    val isAnonymous : Boolean
+    val isAnonymous : Boolean   //column 생성 ? 혹은 판단 후 user.nickname 을 익명으로? 혹은 이넘으로 처리?
 )
