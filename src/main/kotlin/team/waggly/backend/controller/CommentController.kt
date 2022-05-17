5e072df708c06438acc81d99adaab27ab9a097bb
최석영 <suky0713@greencatsoft.com>
2022-05-17 14:28:56 +0900

    댓글 작성, 삭제 (security 없음)

--- src/main/kotlin/team/waggly/backend/controller/CommentController.kt
+++ src/main/kotlin/team/waggly/backend/controller/CommentController.kt
@@ -1,5 +1,7 @@
 package team.waggly.backend.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import team.waggly.backend.dto.commentdto.CommentRequestDto
 import team.waggly.backend.service.CommentService
@@ -7,14 +9,26 @@ import team.waggly.backend.service.CommentService
 @RestController
 class CommentController(private val commentService: CommentService) {
     //댓글 작성
-    @PostMapping("newcomment")
-    fun createComment(@RequestBody commentRequestDto: CommentRequestDto) {
-        return commentService.createComment(commentRequestDto)
+    @PostMapping("comment/{postId}")
+    fun createComment(
+        @PathVariable postId: Long,
+        @RequestBody commentRequestDto: CommentRequestDto,
+        @AuthenticationPrincipal userDetailImpl: UserDetailImpl?
+    ): ResponseEntity<Any> {
+        commentService.createComment(postId, commentRequestDto, userDetailImpl)
+        return ResponseEntity
+            .ok()
+            .body(true)
     }
 
     //댓글 삭제
     @DeleteMapping("/comment/{commentId}")
-    fun deleteComment(@PathVariable commentId : Long) {
-        return commentService.deleteComment(commentId)
+    fun deleteComment(
+        @PathVariable commentId: Long,
+        @AuthenticationPrincipal userDetailImpl: UserDetailImpl?
+    ): ResponseEntity<Any> {
+        return ResponseEntity
+            .ok()
+            .body(commentService.deleteComment(commentId, userDetailImpl))
     }
 }
--- src/main/kotlin/team/waggly/backend/service/CommentService.kt
+++ src/main/kotlin/team/waggly/backend/service/CommentService.kt
@@ -1,13 +1,48 @@
 package team.waggly.backend.service
 
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import team.waggly.backend.commomenum.ActiveStatusType
 import team.waggly.backend.dto.commentdto.CommentRequestDto
+import team.waggly.backend.model.Comment
+import team.waggly.backend.repository.CommentRepository
+import team.waggly.backend.repository.PostRepository
 
-class CommentService {
-    fun createComment(commentRequestDto: CommentRequestDto) {
-        TODO("Not yet implemented")
+@Service
+class CommentService(
+    val commentRepository: CommentRepository,
+    val postRepository: PostRepository
+) {
+    fun createComment(
+        postId: Long,
+        commentRequestDto: CommentRequestDto,
+        userDetails: UserDetailsImpl
+    ) {
+        val post = postRepository.findByIdOrNull(postId) ?: throw IllegalArgumentException("해당 게시글이 없습니다.")
+        val user = userDetails.user
+        val comment = Comment(
+            post = post,
+            user = user,
+            description = commentRequestDto.commentDesc
+        )
+        commentRepository.save(comment)
     }
 
-    fun deleteComment(commentId: Long) {
-        TODO("Not yet implemented")
+    fun deleteComment(
+        commentId: Long,
+        userDetails: UserDetailsImpl
+    ) {
+        var comment = commentRepository.findByIdOrNull(commentId) ?: throw IllegalArgumentException("해당 댓글이 없습니다.")
+        val user = userDetails.user
+        if (comment.user != user) {
+            throw Exception("댓글 작성자만 댓글을 삭제할 수 있습니다.")
+        }
+        comment = Comment(
+            post = comment.post,
+            user = comment.user,
+            description = comment.description,
+            activeStatus = ActiveStatusType.INACTIVE
+        )
+        commentRepository.save(comment)
     }
 }
