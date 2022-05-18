4a68286a5c67ac83a126fed3d8f7bef8ce353b8e
59 <5.9.devv@gmail.com>
2022-05-18 13:27:35 +0900

    리팩토링 + 이미지업로드 기능 업데이트

--- src/main/kotlin/team/waggly/backend/dto/postDto/UpdatePostRequestDto.kt
+++ src/main/kotlin/team/waggly/backend/dto/postDto/UpdatePostRequestDto.kt
@@ -1,13 +1,16 @@
 package team.waggly.backend.dto.postDto
 
+import org.springframework.web.multipart.MultipartFile
 import team.waggly.backend.commomenum.CollegeType
 import team.waggly.backend.model.Post
 import java.time.LocalDateTime
 
 data class UpdatePostRequestDto(
-    val title: String?,
-    val description: String?,
-    val college: CollegeType?,
+        val title: String?,
+        val description: String?,
+        val college: CollegeType?,
+        val file: List<MultipartFile>?,
+        val deleteTargetId: List<String>?,
 ) {
     fun updateEntity(post: Post) {
         post.title = title ?: post.title
