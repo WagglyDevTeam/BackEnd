defb54a9b95b8474c0ffb74cc488229374434e4e
59 <5.9.devv@gmail.com>
2022-06-03 00:33:18 +0900

    merge

--- src/main/kotlin/team/waggly/backend/model/Comment.kt
+++ src/main/kotlin/team/waggly/backend/model/Comment.kt
@@ -22,10 +22,11 @@ class Comment(
     @Column(columnDefinition = "VARCHAR(255)")
     val description: String,
 
-    @Column
+    @Column(columnDefinition = "TINYINT(1)")
     var isAnonymous: Boolean = false,
 
     @Column(columnDefinition = "VARCHAR(10)")
+    @Enumerated(EnumType.STRING)
     var activeStatus: ActiveStatusType = ActiveStatusType.ACTIVE,
 
     @Column
