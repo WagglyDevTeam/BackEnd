4c4b2a66154a65dbe44f94e82ebe963b9245b5e6
SeongeunYang <tnvjaos1826@gmail.com>
2022-06-08 23:07:28 +0900

    s3 config 설정 수정

--- src/main/kotlin/team/waggly/backend/config/AmazonS3Config.kt
+++ src/main/kotlin/team/waggly/backend/config/AmazonS3Config.kt
@@ -2,6 +2,7 @@ package team.waggly.backend.config
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
@@ -10,21 +11,14 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AmazonS3Config {
-    @Value("\${cloud.aws.credentials.accessKey}")
-    lateinit var accessKey: String
-
-    @Value("\${cloud.aws.credentials.secretKey}")
-    lateinit var secretKey: String
-
     @Value("\${cloud.aws.region.static}")
     lateinit var region: String
 
     @Bean
     fun amazonS3Client(): AmazonS3Client {
-        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+                .withCredentials(DefaultAWSCredentialsProviderChain())
                 .build() as AmazonS3Client
     }
 }
