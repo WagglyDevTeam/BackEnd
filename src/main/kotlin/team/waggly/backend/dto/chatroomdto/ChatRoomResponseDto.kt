0ec16c50688f4d3acd0d747278facbd70d6230e2
최석영 <suky0713@greencatsoft.com>
2022-06-15 11:26:02 +0900

    내 채팅방 조회 V1.2

--- src/main/kotlin/team/waggly/backend/dto/chatroomdto/ChatRoomResponseDto.kt
+++ src/main/kotlin/team/waggly/backend/dto/chatroomdto/ChatRoomResponseDto.kt
@@ -1,13 +1,20 @@
 package team.waggly.backend.dto.chatroomdto
 
-import java.time.LocalDateTime
+import team.waggly.backend.model.ChatParticipantsInfo
 
 data class ChatRoomResponseDto(
-    val chatRoomId: Int,
+    val chatRoomId: Long?,
     val opponentNickname: String,
     val profileImage: String,
-    val chatLastMsg: String,
-    val chatLastMsgTime: LocalDateTime,
+    //val chatLastMsg: String,
+    //val chatLastMsgTime: LocalDateTime,
     val isAlarmOn: Boolean,
-    val unreadMsgCnt: Int
-)
+    //val unreadMsgCnt: Int
+){
+    constructor(chatInfo: ChatParticipantsInfo): this(
+        chatRoomId = chatInfo.chatRoom.id,
+        opponentNickname = chatInfo.opponent.nickName,
+        profileImage = chatInfo.opponent.profileImgUrl,
+        isAlarmOn = chatInfo.isAlarmOn
+    )
+}
