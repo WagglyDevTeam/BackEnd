e24890f06b450c931886f5c9f3bb0b017bf2acf1
59 <5.9.devv@gmail.com>
2022-06-22 22:44:14 +0900

    수정

--- src/main/kotlin/team/waggly/backend/repository/ChatParticipantsInfoRepository.kt
+++ src/main/kotlin/team/waggly/backend/repository/ChatParticipantsInfoRepository.kt
@@ -2,11 +2,12 @@ package team.waggly.backend.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import team.waggly.backend.commomenum.ActiveStatusType
 import team.waggly.backend.model.ChatParticipantsInfo
 import team.waggly.backend.model.User
 
 @Repository
 interface ChatParticipantsInfoRepository : JpaRepository<ChatParticipantsInfo, Long> {
-    fun findAllByUser(user: User): List<ChatParticipantsInfo>?
+    fun findAllByUserAndActiveStatus(user: User, activeStatus: ActiveStatusType): List<ChatParticipantsInfo>
 
 }
--- src/main/kotlin/team/waggly/backend/service/ChatRoomService.kt
+++ src/main/kotlin/team/waggly/backend/service/ChatRoomService.kt
@@ -1,6 +1,7 @@
 package team.waggly.backend.service
 
 import org.springframework.stereotype.Service
+import team.waggly.backend.commomenum.ActiveStatusType
 import team.waggly.backend.dto.chatroomdto.ChatRoomResponseDto
 import team.waggly.backend.model.ChatParticipantsInfo
 import team.waggly.backend.model.User
@@ -13,14 +14,7 @@ class ChatRoomService(
     private val chatParticipantsInfoRepository: ChatParticipantsInfoRepository
 ) {
     fun getAllChatRoom(user: User): List<ChatRoomResponseDto> {
-//        TODO(
-//            "1. chatParticipantsInfo 에서 user = user 인 채팅방 정보 찾아옴" +
-//                    "2. ChatRoomResponseDto 리스트 생성" +
-//                    "3. 찾아온 채팅방 정보에서 ChatRoomResponseDto 생성 후 DTO 리스트에 추가 "
-//        )
-// 생성한 채팅방이 없을 경우 예외가 아니라 다른 처리가 필요할듯
-        val myChatRoomInfoList: List<ChatParticipantsInfo> =
-            chatParticipantsInfoRepository.findAllByUser(user) ?: throw IllegalArgumentException("생성된 채팅방이 없습니다.")
+        val myChatRoomInfoList: List<ChatParticipantsInfo> = chatParticipantsInfoRepository.findAllByUserAndActiveStatus(user, ActiveStatusType.ACTIVE)
 
         val chatRoomResponseDtoList: MutableList<ChatRoomResponseDto> = arrayListOf()
 
