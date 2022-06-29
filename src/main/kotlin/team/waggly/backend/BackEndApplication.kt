69ce107042e2fa73e8e0b2c45527f200270bd206
59 <5.9.devv@gmail.com>
2022-06-29 13:07:23 +0900

    modify user service

--- src/main/kotlin/team/waggly/backend/BackEndApplication.kt
+++ src/main/kotlin/team/waggly/backend/BackEndApplication.kt
@@ -2,6 +2,8 @@ package team.waggly.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 class BackEndApplication
--- src/main/kotlin/team/waggly/backend/service/UserService.kt
+++ src/main/kotlin/team/waggly/backend/service/UserService.kt
@@ -8,20 +8,26 @@ import team.waggly.backend.dto.myPageDto.*
 import team.waggly.backend.model.User
 import team.waggly.backend.repository.UserRepository
 import team.waggly.backend.service.awsS3.S3Uploader
+import team.waggly.backend.service.tika.TikaService
 import javax.transaction.Transactional
 
 @Service
 class UserService(
-        private val userRepository: UserRepository,
-        private val s3Uploader: S3Uploader,
-        private val passwordEncoder: BCryptPasswordEncoder,
+    private val userRepository: UserRepository,
+    private val s3Uploader: S3Uploader,
+    private val passwordEncoder: BCryptPasswordEncoder,
+    private val tikaService: TikaService,
 ) {
     @Transactional
     fun updateUserProfile(user: User, updateUserProfileRequestDto: UpdateUserProfileRequestDto): UpdateUserProfileDto {
         val updateUser = userRepository.findByIdOrNull(user.id!!)
                 ?: throw java.lang.IllegalArgumentException("해당 유저가 존재하지 않습니다.")
+        if (!tikaService.typeCheck(updateUserProfileRequestDto.profileImg)) {
+            throw IllegalArgumentException("올바른 파일 형식이 아닙니다. (.jpg, .jpeg, .gif, .png)")
+        }
+
         updateUser.nickName = updateUserProfileRequestDto.nickname
-        updateUser.profileImgUrl = s3Uploader.upload(updateUserProfileRequestDto.profileImg)
+        updateUser.profileImgUrl = s3Uploader.upload(updateUserProfileRequestDto.profileImg, tikaService.mimeType(updateUserProfileRequestDto.profileImg))
 
         return UpdateUserProfileDto(
                 updateUser.profileImgUrl,
