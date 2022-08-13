9f94327c81683db413f4b34f591e6b53921a9760
SeongeunYang <tnvjaos1826@gmail.com>
2022-08-13 19:52:21 +0900

    skipMatcher 수정

--- src/main/kotlin/team/waggly/backend/security/SecurityConfig.kt
+++ src/main/kotlin/team/waggly/backend/security/SecurityConfig.kt
@@ -2,6 +2,7 @@ package team.waggly.backend.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
@@ -66,16 +67,16 @@ class SecurityConfig(
 
     @Bean
     fun jwtAuthFilter(): JwtAuthFilter {
-        val skipPathList = mutableListOf<Pair<HttpMethodEnum, String>>()
+        val skipPathList = mutableListOf<Pair<HttpMethod, String>>()
 
-        skipPathList.add(Pair(HttpMethodEnum.GET, "/h2-console/**"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/h2-console/**"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/user/signup"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/user/email"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/major"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/user/nickname"))
-        skipPathList.add(Pair(HttpMethodEnum.POST,"/user/email/certification"))
-        skipPathList.add(Pair(HttpMethodEnum.GET,"/board/home"))
+        skipPathList.add(Pair(HttpMethod.GET, "/h2-console/**"))
+        skipPathList.add(Pair(HttpMethod.POST,"/h2-console/**"))
+        skipPathList.add(Pair(HttpMethod.POST,"/user/signup"))
+        skipPathList.add(Pair(HttpMethod.POST,"/user/email"))
+        skipPathList.add(Pair(HttpMethod.POST,"/major"))
+        skipPathList.add(Pair(HttpMethod.POST,"/user/nickname"))
+        skipPathList.add(Pair(HttpMethod.POST,"/user/email/certification"))
+        skipPathList.add(Pair(HttpMethod.GET,"/board/home"))
 
         val matcher = FilterSkipMatcher(skipPathList, "/**")
 
