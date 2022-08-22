8c7e772d80a38414ad5a37c9c9e88b72ece732c9
SeongeunYang <tnvjaos1826@gmail.com>
2022-08-22 23:06:29 +0900

    게시글 작성 API 정리

--- src/main/kotlin/team/waggly/backend/controller/PostController.kt
+++ src/main/kotlin/team/waggly/backend/controller/PostController.kt
@@ -1,17 +1,13 @@
 package team.waggly.backend.controller
 
-import org.springframework.data.domain.Pageable
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.BindingResult
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
-import team.waggly.backend.commomenum.CollegeType
-import team.waggly.backend.dto.post.*
 import team.waggly.backend.dto.ResponseDto
+import team.waggly.backend.dto.post.*
 import team.waggly.backend.exception.CustomException
-import team.waggly.backend.model.User
 import team.waggly.backend.security.UserDetailsImpl
 import team.waggly.backend.service.PostService
 import javax.validation.Valid
@@ -48,22 +44,15 @@ class PostController(
 
     // 게시글 작성
     @PostMapping
-    fun createPost(@AuthenticationPrincipal userDetailsImpl: UserDetailsImpl,
-                   @Valid @ModelAttribute postCreateDto: CreatePostRequestDto,
-                   bindingResult: BindingResult): ResponseDto<Any> {
-        if (bindingResult.hasErrors()) {
-            val msg: MutableList<String> = arrayListOf()
-            bindingResult.allErrors.forEach {
-                val field = it as FieldError
-                val message = it.defaultMessage
-                msg.add("${field.field} : $message")
-            }
-            val result = bindingResult.allErrors.map { error ->
-                CustomException.ValidatorExceptionReturnType(error.code!!, error.defaultMessage!!)
-            }
-            return ResponseDto(CustomException.ValidatorException(result))
-        }
-        return ResponseDto(postService.createPost(postCreateDto, userDetailsImpl), HttpStatus.CREATED.value())
+    fun createPost(
+            @AuthenticationPrincipal userDetailsImpl: UserDetailsImpl,
+            @Valid @RequestBody postCreateDto: CreatePostRequestDto,
+    ): ResponseDto<Any> {
+        val user = userDetailsImpl.user
+        return ResponseDto(
+                postService.createPost(postCreateDto, user),
+                HttpStatus.CREATED.value()
+        )
     }
 
     // 게시글 수정
