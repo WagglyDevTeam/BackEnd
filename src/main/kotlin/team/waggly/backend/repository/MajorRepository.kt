f22b59c8d5ca4b2dc573d8e45fbdc22cfd846d0c
59 <5.9.devv@gmail.com>
2022-08-30 12:52:53 +0900

    학교 이름으로 학과리스트 조회

--- src/main/kotlin/team/waggly/backend/repository/MajorRepository.kt
+++ src/main/kotlin/team/waggly/backend/repository/MajorRepository.kt
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
 import team.waggly.backend.model.Major
+import team.waggly.backend.model.Post
+import team.waggly.backend.model.PostLike
+import team.waggly.backend.model.University
 
 @Repository
 interface MajorRepository : JpaRepository<Major, Long> {
+    fun findAllByUniversityOrderByMajorNameAsc(university: University): List<Major>?
 }
 
